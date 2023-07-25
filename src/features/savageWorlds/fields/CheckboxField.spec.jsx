20c7d273f7d093d834b1d56d9994a2bf6e965caf
Dmitry Kutsenko <d2emonium@gmail.com>
2023-07-25 12:10:02 +0300

    Update tests for CheckboxField

--- src/features/savageWorlds/fields/CheckboxField.spec.jsx
+++ src/features/savageWorlds/fields/CheckboxField.spec.jsx
@@ -1,9 +1,55 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event';
 import CheckboxField from './CheckboxField';
 
 describe('CheckboxField', () => {
-  it('renders unchecked checkbox', () => {
+  it('renders checked', async () => {
+    render(<CheckboxField
+      type="checkbox"
+      label="checkbox label"
+      value={true}
+    />);
+
+    const label = screen.getByText('checkbox label');
+    expect(label).toBeInTheDocument();
+
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox.type).toEqual('checkbox');
+    expect(checkbox.checked).toBeTruthy();
+  });
+
+  it('renders unchecked', async () => {
+    render(<CheckboxField
+      type="checkbox"
+      label="checkbox label"
+      value={false}
+    />);
+
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox.checked).toBeFalsy();
+  });
+
+  it('handles checked click', async () => {
+    const user = userEvent.setup();
+    const onChange = jest.fn();
+
+    render(<CheckboxField
+      type="checkbox"
+      label="checkbox label"
+      value={true}
+      onChange={onChange}
+    />);
+
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox.checked).toBeTruthy();
+
+    await user.click(checkbox);
+
+    expect(onChange).toBeCalled();
+    expect(onChange.mock.calls[0][0]).toBeFalsy();
+  });
+
+  it('handles unchecked click', async () => {
     const user = userEvent.setup();
     const onChange = jest.fn();
 
@@ -13,43 +59,99 @@ describe('CheckboxField', () => {
       value={false}
       onChange={onChange}
     />);
-    const label = screen.getByText('checkbox label');
-    expect(label).toBeInTheDocument();
 
     const checkbox = screen.getByRole('checkbox');
-    expect(checkbox.type).toEqual('checkbox');
     expect(checkbox.checked).toBeFalsy();
 
-    userEvent.click(checkbox);
-    userEvent.click(label);
-    fireEvent.change(checkbox, {target: {value: true}})
+    await user.click(checkbox);
 
     expect(onChange).toBeCalled();
+    expect(onChange.mock.calls[0][0]).toBeTruthy();
   });
 
-  it('renders checked checkbox', () => {
+  it('handles unhandled click', async () => {
+    const user = userEvent.setup();
+
     render(<CheckboxField
       type="checkbox"
       label="checkbox label"
       value={true}
     />);
-    const label = screen.getByText('checkbox label');
-    expect(label).toBeInTheDocument();
 
     const checkbox = screen.getByRole('checkbox');
-    expect(checkbox.type).toEqual('checkbox');
+    expect(checkbox.checked).toBeTruthy();
+
+    await user.click(checkbox);
+
     expect(checkbox.checked).toBeTruthy();
   });
+});
 
-  it('renders radio', () => {
+
+describe('CheckboxField with radio type', () => {
+  it('renders checked', async () => {
     render(<CheckboxField
       type="radio"
       label="radio label"
+      value={true}
     />);
+
     const label = screen.getByText('radio label');
     expect(label).toBeInTheDocument();
 
     const checkbox = screen.getByRole('radio');
     expect(checkbox.type).toEqual('radio');
+    expect(checkbox.checked).toBeTruthy();
   });
+
+  it('renders unchecked', async () => {
+    render(<CheckboxField
+      type="radio"
+      label="radio label"
+      value={false}
+    />);
+
+    const checkbox = screen.getByRole('radio');
+    expect(checkbox.checked).toBeFalsy();
+  });
+
+  it('handles click', async () => {
+    const user = userEvent.setup();
+    const onChange = jest.fn();
+
+    render(<CheckboxField
+      type="radio"
+      value={false}
+      onChange={onChange}
+    />);
+
+    const checkbox = screen.getByRole('radio');
+    expect(checkbox.checked).toBeFalsy();
+
+    await user.click(checkbox);
+
+    expect(onChange).toBeCalled();
+    expect(onChange.mock.calls[0][0]).toBeTruthy();
+  });
+
+  /*
+  it('handles checked click', async () => {
+    const user = userEvent.setup();
+    const onChange = jest.fn();
+
+    render(<CheckboxField
+      type="radio"
+      value={true}
+      onChange={onChange}
+    />);
+
+    const checkbox = screen.getByRole('radio');
+    expect(checkbox.checked).toBeTruthy();
+
+    await user.click(checkbox);
+
+    expect(onChange).toBeCalled();
+    expect(onChange.mock.calls[0][0]).toBeTruthy();
+  });
+  */
 });
