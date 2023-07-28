d525acc6ee104e87dc7ebf22beefcecf4c063f79
Dmitry Kutsenko <d2emonium@gmail.com>
2023-07-28 11:17:15 +0300

    Add tests for number field

--- src/features/savageWorlds/fields/CommonField.spec.jsx
+++ src/features/savageWorlds/fields/CommonField.spec.jsx
@@ -70,3 +70,72 @@ describe('CommonField', () => {
       .toThrowError();
   });
 });
+
+describe('CommonField as number', () => {
+  it('renders text field', async () => {
+    render(<CommonField
+      placeholder="label"
+      type="number"
+      value={1}
+    />);
+
+    const field = screen.getByRole('spinbutton');
+    expect(field.placeholder).toEqual('label');
+    expect(field.type).toEqual('number');
+    expect(field.value).toEqual('1');
+  });
+
+  it('handles change', async () => {
+    const user = userEvent.setup();
+    const onChange = jest.fn();
+
+    render(<CommonField
+      type="number"
+      value={1}
+      onChange={onChange}
+    />);
+
+    const field = screen.getByRole('spinbutton');
+    expect(field.value).toEqual('1');
+
+    await user.type(field, '1');
+
+    expect(onChange).toBeCalled();
+    expect(onChange.mock.calls[0][0]).toEqual('11');
+  });
+
+  it('don\'t run onChange when readOnly', async () => {
+    const user = userEvent.setup();
+    const onChange = jest.fn();
+
+    render(<CommonField
+      type="number"
+      readOnly
+      value={1}
+      onChange={onChange}
+    />);
+
+    const field = screen.getByRole('spinbutton');
+    expect(field.value).toEqual('1');
+
+    await user.type(field, '1');
+
+    expect(onChange).not.toBeCalled();
+  });
+
+  it('throws no error on no onChange', async () => {
+    const user = userEvent.setup();
+
+    render(<CommonField
+      type="number"
+      value={1}
+    />);
+
+    const field = screen.getByRole('spinbutton');
+
+    await expect(user.type(field, '1'))
+      .resolves
+      .not
+      .toThrowError();
+  });
+});
